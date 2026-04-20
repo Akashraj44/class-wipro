8481d7f2d98402b6d5db89ff383c2a4efd773508
Akash Raj <akashraj23864@gmail.com>
2026-04-20 15:35:54 +0530

    Java programming

---
+++ FIRST_SELENIUM/src/First_package/First_class.java
@@ -0,0 +1,11 @@
+package First_package;
+
+public class First_class {
+
+	public static void main(String[] args) {
+		System.out.println("hello student");
+		// TODO Auto-generated method stub
+
+	}
+
+}
